d7b1887f4fc18cc26d10006e2f88ab21b194d11f
David Bárcenas <daveepro@outlook.com>
2020-02-13 19:18:48 -0600

    feat(channel): add view for podcast item

---
+++ pages/channel.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class channel extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Channel</h2>
+      </div>
+    );
+  }
+}
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Link from "next/link";
 import "isomorphic-fetch";
 
 export default class index extends Component {
@@ -12,9 +13,69 @@ export default class index extends Component {
     const { channels } = this.props;
     return (
       <>
-        {channels.map(channel => (
-          <p key={channel.id}>{channel.title}</p>
-        ))}
+        <header>Podcasts</header>
+
+        <div className="channels">
+          {channels.map(channel => (
+            <Link href="/channel" key={channel.id}>
+              <a className="channel">
+                <figure>
+                  <img
+                    src={channel.urls.logo_image.original}
+                    alt={channel.title}
+                  />
+                  <figcaption>{channel.title}</figcaption>
+                </figure>
+              </a>
+            </Link>
+          ))}
+        </div>
+
+        <style jsx>{`
+          header {
+            color: #fff;
+            background: #8756ca;
+            font-weight: bold;
+            padding: 15px;
+            text-align: center;
+          }
+          .channels {
+            display: grid;
+            grid-gap: 5px;
+            padding: 15px;
+            grid-template-columns: repeat(auto-fill, minmax(160px, 1fr));
+          }
+          .channel {
+            display: block;
+            border-radius: 3px;
+            box-shadow: 0px 2px 6px rgba(0, 0, 0, 0.15);
+            margin-bottom: 0.5em;
+          }
+          .channel figure {
+            margin: 0;
+            position: relative;
+            overflow: hidden;
+          }
+          .channel img {
+            width: 100%;
+          }
+          .channel figcaption {
+            box-shadow: 0 -20px 40px rgba(0, 0, 0, 0.65) inset;
+            box-sizing: border-box;
+            position: absolute;
+            left: 0;
+            bottom: 0;
+            color: #fff;
+            font-size: 0.9em;
+            margin: 0;
+            padding: 0.5em;
+            width: 100%;
+            z-index: 2;
+          }
+          .channel:hover {
+            opacity: 0.9;
+          }
+        `}</style>
 
         <style jsx global>{`
           body {
