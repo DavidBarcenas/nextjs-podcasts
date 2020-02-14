288604763c2b8af342aa4fed21c8949804e33efc
David Bárcenas <daveepro@outlook.com>
2020-02-13 18:48:03 -0600

    feat(isomorphic-fetch): add library to support fetch in ssr - consume audioboom service

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,34 @@
 import React, { Component } from "react";
+import "isomorphic-fetch";
 
 export default class index extends Component {
+  static async getInitialProps() {
+    let req = await fetch("https://api.audioboom.com/channels/recommended"),
+      { body: channels } = await req.json();
+    return { channels };
+  }
+
   render() {
+    const { channels } = this.props;
     return (
-      <div>
-        <h1>Hello World</h1>
-      </div>
+      <>
+        {channels.map(channel => (
+          <p key={channel.id}>{channel.title}</p>
+        ))}
+
+        <style jsx global>{`
+          body {
+            margin: 0;
+            font-family: arial, sans-serif;
+            background: #fffffa;
+          }
+
+          a {
+            text-decoration: none;
+            color: #292929;
+          }
+        `}</style>
+      </>
     );
   }
 }
